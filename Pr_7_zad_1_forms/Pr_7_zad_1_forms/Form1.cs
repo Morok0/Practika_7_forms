1fbbb6da85cbac85971ea5c9d2612f145881f4c7
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-15 19:55:37 +0300

    Add files via upload

---
+++ Pr_7_zad_1_forms/Pr_7_zad_1_forms/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace Pr_7_zad_1_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line = " ";
+            line = Convert.ToString(textBox1.Text);
+            char x;
+            char y;
+            x = char.Parse(textBox2.Text);
+            y = char.Parse(textBox3.Text);
+
+            int countX = line.Where(a => a == x).Count();
+            int countY = line.Where(a => a == y).Count();
+            if (line.Contains((char)x) || line.Contains((char)y))
+            {
+                if (countX > countY)
+                textBox4.Text= Convert.ToString("Больше всего встречается="+x);
+            else if (countY > countX)
+                textBox4.Text = Convert.ToString("Больше всего встречается=" + y);
+            else
+                textBox4.Text = Convert.ToString("Они встречаются одинаково");
+            }
+            else
+                textBox4.Text = Convert.ToString("Таких символов нет");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = ""; textBox4.Text = "";
+        }
+    }
+}
---
+++ Pr_7_zad_2_forms/Pr_7_zad_2_forms/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_7_zad_2_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int buf = 0;
+            string str = "";
+            string[] s;
+            str = textBox1.Text;
+            s = str.Split();
+            for (int i = 0; i < s.Length; i++)
+            {
+                buf = s[i].Length;
+                for (int j = 0; j < s[i].Length; j++)
+                {
+                    if (char.IsUpper(s[i][j]))
+                    {
+                        for (int z = 0; z < s[i].Length; z++)
+                        {
+                            if (s[i][z] != ',')
+                                    if (s[i][z] != '.')
+                                               textBox2.Text+=Convert.ToString(s[i][z]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = " "; textBox1.Text = "";
+        }
+    }
+}
